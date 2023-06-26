ec97b1dd18c9ad98aedde94bbbeed4f2f345f745
Kasko <t.kasko@itransition.com>
2023-06-26 16:53:37 +0300

    Initial commit

---
+++ src/main/java/com/ietswise/IELTSWiseApplication.java
@@ -0,0 +1,12 @@
+package com.ietswise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IELTSWiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IELTSWiseApplication.class, args);
+    }
+}
