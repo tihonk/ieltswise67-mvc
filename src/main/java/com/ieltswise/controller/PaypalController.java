fb015c37a7df7436438c846fc1a7eb8c1bc31762
Kasko <t.kasko@itransition.com>
2024-01-24 16:30:32 +0300

    Payment redirect url

--- src/main/java/com/ieltswise/controller/PaypalController.java
+++ src/main/java/com/ieltswise/controller/PaypalController.java
@@ -3,10 +3,7 @@ package com.ieltswise.controller;
 import com.ieltswise.service.PayPalPaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.OK;
@@ -21,6 +18,7 @@ public class PaypalController {
         this.payPalService = payPalService;
     }
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/paymentLink")
     public ResponseEntity<String> getPaymentLink(@RequestParam("successUrl") String successUrl,
                                                  @RequestParam("cancelUrl") String cancelUrl,
