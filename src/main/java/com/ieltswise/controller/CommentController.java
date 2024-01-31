b1771b8fe377ea5494ae7a103fd4f6565d805eb9
Kasko <t.kasko@itransition.com>
2024-01-31 16:47:00 +0300

    CrossOrigin annotation

--- src/main/java/com/ieltswise/controller/CommentController.java
+++ src/main/java/com/ieltswise/controller/CommentController.java
@@ -7,11 +7,7 @@ import com.ieltswise.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,12 +23,14 @@ public class CommentController {
         this.commentRepository = commentRepository;
     }
 
+    @CrossOrigin(origins = "*")
     @GetMapping
     ResponseEntity<List<StudentComment>> getAllComments() {
         List<StudentComment> comments = commentRepository.findAll();
         return new ResponseEntity<>(comments, HttpStatus.OK);
     }
 
+    @CrossOrigin(origins = "*")
     @PostMapping()
     ResponseEntity<?> createComment(@RequestBody StudentCommentDto commentDto) {
         try {
