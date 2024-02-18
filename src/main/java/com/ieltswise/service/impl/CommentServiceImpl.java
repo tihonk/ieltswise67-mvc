9626d4693820122a50712a69fd7c8d38a736cca7
Kasko <t.kasko@itransition.com>
2024-02-18 22:31:18 +0300

    Updating comment saving functionality

--- src/main/java/com/ieltswise/service/impl/CommentServiceImpl.java
+++ src/main/java/com/ieltswise/service/impl/CommentServiceImpl.java
@@ -30,18 +30,20 @@ public class CommentServiceImpl implements CommentService {
     }
 
     public StudentComment createComment(StudentCommentDto studentCommentDto) {
-        String email = studentCommentDto.getStudentEmail();
+        String email = studentCommentDto.getEmail();
         UserLessonData lessonData = userLessonDataRepository.findByEmail(email);
+        System.out.println(email +" email is trying to add a comment, here is the user data: " + lessonData);
 
         if (lessonData != null && lessonData.getAllPaidLessons() > 0) {
             StudentComment comment = mapper.mapToStudentComment(studentCommentDto);
             if (lessonData.getName() == null) {
-                comment.setStudentName("User" + lessonData.getUserId());
+                comment.setName("User" + lessonData.getUserId());
             } else {
-                comment.setStudentName(lessonData.getName());
+                comment.setName(lessonData.getName());
             }
             comment.setCreated(LocalDateTime.now());
             try {
+                System.out.println("The comment is ready to be saved:" + comment);
                 return commentRepository.save(comment);
             } catch (Exception e) {
                 throw new RuntimeException("Failed to save comment", e);
